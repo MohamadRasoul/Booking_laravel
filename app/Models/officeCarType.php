0d1b845ea095ee4f4a3130bb80b2c479bff9ba29
Mohamad_RA <mralmaahlol@gmail.com>
2023-06-10 16:08:09 +0300

    Add officeCarType Model

---
+++ app/Models/officeCarType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class officeCarType extends Pivot
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected $casts = [];
+
+    ########## Relations ##########
+
+
+
+}
---
+++ database/seeders/OfficeCarTypeSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class OfficeCarTypeSeeder extends Seeder
+{
+    
+    
+    public function run(): void
+    {
+        //  \App\Models\OfficeCarTypeSeeder::factory(10)->create();
+        
+    }
+}
