d294e4a3848bf8311141f477b09a15c9e77b1dfa
Mohamad_RA <mralmaahlol@gmail.com>
2023-06-19 14:52:33 +0300

    Create TestSeeder.php

--- database/seeders/TestSeeder.php
+++ database/seeders/TestSeeder.php
@@ -5,20 +5,20 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class TestSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        $this->callWith([
-            TestSeeder::class
-        ]);
+
 
         $this->call([
             citySeeder::class,
             AdminSeeder::class,
         ]);
+
+
     }
 }
